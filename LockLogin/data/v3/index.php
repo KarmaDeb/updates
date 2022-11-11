b024df3fcaf6b8c7a580d5388fdfc029dfeaf823
KarmaDev <karmaconfigs@gmail.com>
2022-11-11 14:53:22 +0100

    Updated v3 checksum

--- LockLogin/data/v3/index.php
+++ LockLogin/data/v3/index.php
@@ -11,7 +11,7 @@ if ($type == 'adler') {
 	echo "{\n";
 	echo "	\"Apache Commons\": 3916456401\n",
 	echo "	\"Google Authenticator\": 293225421,\n";
-	echo "	\"Google Gson\": 2593554311,\n";
+	echo "	\"Google Guava\": 4241935111,\n";
 	echo "	\"Log4j\": 217741531,\n";
 	echo "	\"Log4j Web\": 993548242,\n";
 	echo "	\"Remote Messaging\": 3411176868,\n";
@@ -21,7 +21,7 @@ if ($type == 'adler') {
 	echo "{\n";
 	echo "	\"Apache Commons\": 2418094782\n",
 	echo "	\"Google Authenticator\": 868364232,\n";
-	echo "	\"Google Gson\": 2192529993,\n";
+	echo "	\"Google Guava\": 4146188870,\n";
 	echo "	\"Log4j\": 556285814,\n";
 	echo "	\"Log4j Web\": 308611953,\n";
 	echo "	\"Remote Messaging\": 4022847772,\n";
